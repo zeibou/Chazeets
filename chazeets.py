0744e42db7bd63e98a8d3734affe9f4db9230b3e
nicolas.seibert <zeibou@gmail.com>
2020-03-30 18:19:42 -0400

    add date range frame

--- chazeets.py
+++ chazeets.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import logging
 import threading
 import PySimpleGUI as sg
@@ -5,27 +6,32 @@ import PySimpleGUI as sg
 import configuration as cfg
 from chase_scraper import ChaseScraper
 
+KEY_DATES_FROM = "date_from-key"
+KEY_DATES_TO = "date_to-key"
+
 KEY_CHASE_USERNAME = "chase_username-key"
 KEY_CHASE_PASSWORD = "chase_password-key"
-KEY_CHASE_SIGNIN = "chase_signin-key"
+KEY_CHASE_SIGNIN = "chase_button_signin-key"
 
-FONT_TITLE = ("Gill Sans", 35)
+KEY_RUN = "button_run-key"
+
+FONT_TITLE = ("Gill Sans Bold", 35)
+FONT_GROUP = ("Gill Sans", 25)
 FONT_TEXT = ("Gill Sans Light", 16)
 
-sg.SetOptions(font=FONT_TEXT)
-
-config = cfg.get_configuration()
-
-# sg.preview_all_look_and_feel_themes()
+FORMAT_DATE = "%Y-%m-%d"
 
 
 class ScraperThread:
     scraper = None
     thread = None
 
+    def __init__(self, config):
+        self.config = config
+
     def get_scraper(self):
         if not self.scraper:
-            self.scraper = ChaseScraper(config.chromedriver_path)
+            self.scraper = ChaseScraper(self.config.chromedriver_path)
         return self.scraper
 
     def logon(self, username, password):
@@ -38,60 +44,98 @@ class ScraperThread:
         self.thread = threading.Thread(target=target)
         self.thread.start()
 
-    def quit(self, timeout=0):
+    def close(self, timeout=0):
         if self.scraper:
             self.scraper.quit(timeout)
 
 
-# ------ Chase Frame Definition ------ #
-sg.theme('DarkBlue15')
-chase_frame = sg.Frame("Chase", font=FONT_TITLE, relief=sg.RELIEF_FLAT, layout=
-[
+def main():
+    config = cfg.get_configuration()
+
+    now = dt.datetime.now()
+    first_day_of_this_month = dt.datetime(now.year, now.month, 1)
+    date_to = first_day_of_this_month - dt.timedelta(days=1)
+    date_from = dt.datetime(date_to.year, date_to.month, 1)
+
+    sg.SetOptions(font=FONT_TEXT)
+
+    # ------ Date Range Definition ------ #
+    sg.theme('DarkTeal7')
+    date_range_frame = sg.Frame("", relief=sg.RELIEF_FLAT, element_justification='left', layout=
     [
-        sg.Text("Login: ", size=(15, 1), justification='right'),
-        sg.InputText(key=KEY_CHASE_USERNAME, default_text=config.chase_username)
-    ],
+        [sg.Text("From:"),
+         sg.InputText(key=KEY_DATES_FROM, size=(10, 1), justification='center',
+                      default_text=date_from.strftime(FORMAT_DATE)),
+         sg.CalendarButton("Pick", target=KEY_DATES_FROM, format=FORMAT_DATE),
+         sg.Text("", size=(3, 1)),
+         sg.Text("To:"),
+         sg.InputText(key=KEY_DATES_TO, size=(10, 1), justification='center', default_text=date_to.strftime(FORMAT_DATE)),
+         sg.CalendarButton("Pick", target=KEY_DATES_TO, format=FORMAT_DATE)]
+    ])
+
+    # ------ Chase Frame Definition ------ #
+    sg.theme('DarkBlue15')
+    chase_logon_frame = sg.Frame("Logon", font=FONT_GROUP, relief=sg.RELIEF_RIDGE, layout=
     [
-        sg.Text("Password: ", size=(15, 1), justification='right'),
-        sg.InputText(key=KEY_CHASE_PASSWORD, password_char='*')]
-    ,
+        [
+            sg.Text("Login: ", size=(15, 1), justification='right'),
+            sg.InputText(key=KEY_CHASE_USERNAME, size=(20, 1), justification='center', default_text=config.chase_username)
+        ],
+        [
+            sg.Text("Password: ", size=(15, 1), justification='right'),
+            sg.InputText(key=KEY_CHASE_PASSWORD, size=(20, 1), justification='center', password_char='*')
+        ],
+        [
+            sg.Text("", size=(25, 1)), sg.Button("Sign In only", key=KEY_CHASE_SIGNIN)
+        ]
+    ])
+
+    def account_key(account):
+        return f"account_{account}-key"
+
+    chase_accounts_frame = sg.Frame("Accounts", font=FONT_GROUP, relief=sg.RELIEF_RIDGE, layout=
+    [[sg.Checkbox(account, key=account_key(account), default=True)] for account in config.chase_accounts])
+
+    chase_frame = sg.Frame("Chase", font=FONT_TITLE, relief=sg.RELIEF_FLAT, layout=
     [
-        sg.Button("Sign In", key=KEY_CHASE_SIGNIN)
+        [chase_logon_frame],
+        [chase_accounts_frame]
+    ])
+
+    # ------ Google Sheets Frame Definition ------ #
+    sg.ChangeLookAndFeel('DarkAmber')
+    gsheets_frame = sg.Frame("Sheets", font=FONT_TITLE, relief=sg.RELIEF_FLAT, layout=
+    [
+        [sg.Text("Sheets contents", justification='center')]
+    ])
+
+    layout = [
+        [date_range_frame],
+        [chase_frame, sg.VerticalSeparator(), gsheets_frame],
+        [sg.Button('Run', key=KEY_RUN, size=(20, 1)), sg.Text("", size=(80, 1)), sg.Button('Exit')]
     ]
-])
+
+    window = sg.Window("Chazeets", layout, keep_on_top=True, element_justification='center')
 
 
-def handle_chase_events(event, values):
-    if event == KEY_CHASE_SIGNIN:
-        scraper_thread.logon(values[KEY_CHASE_USERNAME], values[KEY_CHASE_PASSWORD])
+    scraper_thread = ScraperThread(config)
+    while True:
+        event, values = window.read(timeout=100)
+        if event == '__TIMEOUT__':
+            continue
+        if event in (None, 'Exit'):
+            break
+        if event == KEY_CHASE_SIGNIN:
+            scraper_thread.logon(values[KEY_CHASE_USERNAME], values[KEY_CHASE_PASSWORD])
+        if event == KEY_RUN:
+            pass
+            #scraper_thread.logon(values[KEY_CHASE_USERNAME], values[KEY_CHASE_PASSWORD])
+
+    window.close()
+    scraper_thread.close(0)
 
 
-# ------ Google Sheets Frame Definition ------ #
-sg.ChangeLookAndFeel('DarkAmber')
-gsheets_frame = sg.Frame("Sheets", font=FONT_TITLE, relief=sg.RELIEF_FLAT, layout=
-[
-    [sg.Text("Sheets contents", justification='center')]
-])
+if __name__ == "__main__":
+    main()
 
-layout = [
-    [
-        chase_frame,
-        gsheets_frame
-    ],
-    [
-        sg.Button('Exit', )
-    ]
-]
 
-window = sg.Window("Chazeets", layout, keep_on_top=True)
-
-scraper_thread = ScraperThread()
-while True:
-    event, values = window.read(timeout=100)
-    if event in (None, 'Exit'):
-        break
-    window.Reappear()
-    handle_chase_events(event, values)
-
-window.close()
-scraper_thread.quit(0)

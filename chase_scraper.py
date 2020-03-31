347414ec4c0e4d2d53e14e04308ac00ddb98ca7e
nicolas.seibert <zeibou@gmail.com>
2020-03-31 15:42:32 -0400

    switch to xpath for some elements
    ids are different depending on the type of card / account

--- chase_scraper.py
+++ chase_scraper.py
@@ -17,12 +17,15 @@ CHASE_STATEMENTS_URL = "https://secure01b.chase.com/web/auth/dashboard#/dashboar
 INPUT_LOGON_USERNAME_ID = 'userId-text-input-field'
 INPUT_LOGON_PASSWORD_ID = 'password-text-input-field'
 
-DROPDOWN_ACTIVITY_ID = 'currentDisplayOption'
-DROPDOWN_DATE_RANGE_ID = 'item-8-DATE_RANGE'
+DROPDOWN_ACTIVITY_XPATH = "/html/body/div[2]/div/div[1]/div[2]/main/div[3]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div"
+DROPDOWN_DATE_RANGE_XPATH = '//text()[. = "Choose a date range"]/../..'
+
 INPUT_DATE_FROM_ID = 'input-accountActivityFromDate-validate-input-field'
 INPUT_DATE_TO_ID = 'input-accountActivityToDate-validate-input-field'
 BUTTON_DOWNLOAD_ID = 'download'
 BUTTON_DOWNLOAD_OTHER_ID = 'downloadOtherActivity'
+
+DATE_FORMAT = '%m/%d/%Y'
 
 
 class ChaseScraper:
@@ -31,15 +34,15 @@ class ChaseScraper:
         options.add_argument("--disable-extensions")
         self.driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
-    def logon(self, username, password):
+    def logon(self, username, password, wait_seconds=3):
         self.driver.get(CHASE_LOGIN_URL)
         self._find_by_id(INPUT_LOGON_USERNAME_ID).send_keys(username)
         self._find_by_id(INPUT_LOGON_PASSWORD_ID).send_keys(password)
         self._find_by_id(INPUT_LOGON_PASSWORD_ID).submit()
-        time.sleep(3)
+        time.sleep(wait_seconds)
 
-    def quit(self, cooldown_s=5):
-        time.sleep(cooldown_s)   # give time to finish last action
+    def quit(self, wait_seconds=5):
+        time.sleep(wait_seconds)   # give time to finish last action
         if self.driver:
             self.driver.quit()
             self.driver = None
@@ -47,9 +50,13 @@ class ChaseScraper:
     def download_statement(self, account_id, date_from, date_to):
         logging.info(f"Retrieving statement for account {account_id} from {date_from} to {date_to}")
         self.driver.get(CHASE_STATEMENTS_URL + account_id)
-        activity_element = self._find_by_id(DROPDOWN_ACTIVITY_ID)
+        time.sleep(1)
+
+        activity_element = self._find_by_xpath(DROPDOWN_ACTIVITY_XPATH)
         activity_element.click()
-        date_link_element = self._find_by_id(DROPDOWN_DATE_RANGE_ID)
+        time.sleep(.5)
+
+        date_link_element = self._find_by_xpath(DROPDOWN_DATE_RANGE_XPATH)
         date_link_element.click()
 
         self._find_by_id(INPUT_DATE_FROM_ID).send_keys(date_from)
@@ -57,19 +64,31 @@ class ChaseScraper:
 
         self._find_by_id(BUTTON_DOWNLOAD_ID).click()
         self._find_by_id(BUTTON_DOWNLOAD_OTHER_ID).click()
+        time.sleep(1)
 
-    def _find_by_id(self, elt_id, timeout_s=5):
-        return self._find(self.driver.find_element_by_id, elt_id, timeout_s)
+    def _find_by_id(self, elt_id, timeout_s=10):
+        return self._find(self.driver.find_element_by_id, [elt_id], timeout_s)
+
+    def _find_by_ids(self, elt_id_list, timeout_s=10):
+        return self._find(self.driver.find_element_by_id, elt_id_list, timeout_s)
+
+    def _find_by_xpath(self, xpath, timeout_s=10):
+        return self._find(self.driver.find_element_by_xpath, [xpath], timeout_s)
 
     @staticmethod
-    def _find(finder, elt_id, timeout_s):
+    def _find(finder, elt_id_list, timeout_s):
         max_time = time.time() + timeout_s
         item = None
         while not item and time.time() < max_time:
-            try:
-                item = finder(elt_id)
-            except:
+            for elt_id in elt_id_list:
+                try:
+                    item = finder(elt_id)
+                except Exception as e:
+                    pass
+            if not item:
                 time.sleep(.5)
+        if not item:
+            logging.error(f"Could not find item(s) {elt_id_list}")
         return item
 
 
